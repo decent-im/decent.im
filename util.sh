d560b597f69c8908720fde42022807e6fec11469
Andrey Utkin <andrey_utkin@fastmail.com>
2016-03-24 18:38:21 +0200

    Use escaping-free hexadecimal characters in random_string

--- util.sh
+++ util.sh
@@ -2,5 +2,5 @@
 
 function random_string() {
 RANDOM_BYTES=${1:-16}
-	dd if=/dev/urandom count=1 bs=$RANDOM_BYTES 2>/dev/null | base64
+	dd if=/dev/urandom count=1 bs=$RANDOM_BYTES 2>/dev/null | hexdump -ve '1/1 "%.2x"'
 }
